da8ab9c4dfee26e30837eacaad89635feb5b066e
priyasonar1207 <priyasonar1207@gmail.com>
2021-04-29 18:51:17 +0530

    Day4_Assignment Completed

--- src/com/arraynumbers/Assignment2.java
+++ src/com/arraynumbers/Assignment2.java
@@ -1,57 +1,54 @@
 package com.arraynumbers;
-import java.util.Scanner;
 
 public class Assignment2 {
 
-	public static void main(String[] args) {
+	 String strActualDay[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+     String strExpectedArray[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+    
+		public  void checkequality() {
+         {
+	        		 for(int intIndex= 0; intIndex < strActualDay.length; intIndex++) 
+	        		 
+	        		 {
+	        				 
+	        				 if(strExpectedArray[intIndex].equals("Monday")) {
+	        			         System.out.println("Week Starting");}
+	    		    	 
+	        		         else if(strExpectedArray[intIndex].equals("Tuesday")) {
+	        			          System.out.println("Work Started");}
+    		    	 
+	        		         else if(strExpectedArray[intIndex].equals("Wednesday")) {
+	        			         System.out.println("Work in progress");}
+    		    	 
+	        		         else if(strExpectedArray[intIndex].equals("Thursday")) {
+	        			         System.out.println("Work finish");}
+    		    	 
+	        		         else if(strExpectedArray[intIndex].equals("Friday")) {
+	        			       break;}
+    		    	 
+	        		         else if(strExpectedArray[intIndex].equals("Saturday")) {
+	        			         System.out.println("Party Day");}
+	        		 
+	        		         else if(strExpectedArray[intIndex].equals("Sunday")) {
+	        		        	 continue;}
+	        		          else
+	        			      {
+	        				     System.out.println("Invalid");
+	        			      }
+	        		 }
+	      }
+	 }
 
-     String strActualDay[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-     
-     Scanner sc= new Scanner(System.in);
-     System.out.println("Enter the day You want to Verify:");
-     
-     String strExpectedArray=sc.nextLine();
-     System.out.println("You have Entered:"+strExpectedArray);
-      
-    	switch (strExpectedArray) 
-    	{
-    		     
-    		     case "Sunday":
-    		    	 System.out.println("Holiday");
-    		    	 break;
-    		         
-    		     case "Monday":
-    		    	 System.out.println("Week starting");
-    		         break;
-    		         
-    		     case "Tuesday":
-    		    	 System.out.println("Work started");
-    		         break;
-    		         
-    		     case "Wednesday":
-    		    	 System.out.println("Work in Progress");
-    		         break;
-    		         
-    		     case "Thursday":
-    		    	 System.out.println("Work Finished");
-    		         break;
-    		         
-    		     case "Friday":
-    		    	 System.out.println("Weekend Day");
-    		         break;
-    		         
-    		     case "Saturday":
-    		    	 System.out.println("Party Day");
-    		         break;
-    		         
-    		     default:
-    		            System.out.println("Error");
-    		            break;
-    		         }
-    	}
-}
-    			
-    		    
-    		
-     
-     
+	       public static void main(String args[]) {
+	        	 
+	    	 Assignment2 objAssignment2=new Assignment2();
+	    	 objAssignment2.checkequality();
+   }
+}	
+	    		    
+	    		         
+	            
+	  			
+	    		    
+	    		
+	     
