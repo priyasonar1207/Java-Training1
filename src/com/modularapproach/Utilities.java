16d8df84be3b928b3136f7efe202b3ecd9a5a7af
priyasonar1207 <priyasonar1207@gmail.com>
2021-05-04 18:14:04 +0530

    4-5-2021 Assignment Completed 1)(==)and (.equals) operator 2)Modular Approach 3)Utilities

--- src/com/modularapproach/Utilities.java
+++ src/com/modularapproach/Utilities.java
@@ -65,25 +65,8 @@ int RandomYear= r4.nextInt(strBirthYear.length);
 System.out.println("User Birth year:"+strBirthYear[RandomYear]);
 
 }
+}
 
 
-public static void main(String[] args) {
-	
-	Utilities objUtilities  =new Utilities ();
-	
-	objUtilities.initialiseWebEnvironment("http://www.facebook.com");
-	objUtilities.clickonCreateNewAccount();
-	objUtilities.getRandomFirstName();
-	objUtilities.getRandomSurName();
-	objUtilities.getRandomEmail();
-	objUtilities.getRandomBirthDate();
-	objUtilities.getRandomBirthMonth();
-	objUtilities.getRandomBirthYear();
-	objUtilities.doSignUp("1123","female");
-	objUtilities.clickOnSignUpButton();
-	objUtilities.verifyLoginButtonisDisplayed();
-	objUtilities.closeBrowser();
-	
-}}
 
 
